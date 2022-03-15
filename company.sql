6bb7e85d1372de57b30090a326b8bed885cf8aeb
Varna M V <93851047+varna7@users.noreply.github.com>
2022-03-15 15:06:57 +0530

    Add files via upload

---
+++ company.sql
@@ -0,0 +1,14 @@
+create database company;
+use company;
+create table regions(region_id int,region_name varchar(50),primary key(region_id));
+create table countries(country_id int,country_name varchar(50),region_id int,primary key(country_id),foreign key(region_id) references regions(region_id));
+create table locations(location_id int,street_address varchar(100),postal_code int,city varchar(50),state_province varchar(50),country_id int,primary key(location_id),foreign key(country_id) references countries(country_id));
+create table departments(department_id int,department_name varchar(50),location_id int,primary key(department_id),foreign key(location_id) references locations(location_id));
+create table jobs(job_id int,job_title varchar(20),max_salary int,min_salary int,primary key(job_id));
+create table employees(employee_id int,first_name varchar(5),last_name varchar(5),email varchar(50),phone_number int(10),hire_date varchar(20),job_id int,salary int,manager_id int,department_id int,primary key(employee_id),foreign key(job_id)references jobs(job_id),foreign key(manager_id) references employees(employee_id),foreign key(department_id) references departments(department_id));
+create table dependents(dependent_id int,first_name varchar(50),last_name varchar(5),relationship varchar(20),employee_id int, primary key(dependent_id),foreign key(employee_id) references employees(employee_id));
+select * from regions;
+alter table countries modify column region_id int NOT NULL;
+alter table locations modify column country_id int NOT NULL;
+alter table employees modify column job_id int NOT NULL;
+alter table dependents modify column employee_id int NOT NULL;
